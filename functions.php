2403b8aa43e77f152bee5b51998bb8cb7ff3e5e6
Jeff Mayfield <jeffrey.p.mayfield@gmail.com>
2020-03-14 14:33:02 -0700

    Fixing Child Theme Functions
    Fixing child theme functions. SCSS compilations.

--- functions.php
+++ functions.php
@@ -1,8 +1,13 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-function my_theme_enqueue_styles() {
+
+/**
+ * Include styles from parent and child themes.
+ *
+ * @link https://developer.wordpress.org/themes/advanced-topics/child-themes/
+ */
+function powdermountain_enqueue_styles() {
  
-    $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    $parent_style = 'parent-style';
  
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
@@ -11,3 +16,37 @@ function my_theme_enqueue_styles() {
         wp_get_theme()->get('Version')
     );
 }
+
+add_action( 'wp_enqueue_scripts', 'powdermountain_enqueue_styles' );
+
+
+/**
+ * Register widget areas.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function powdermountain_sidebar_registration() {
+
+	// Arguments used in all register_sidebar() calls.
+	$shared_args = array(
+		'before_title'  => '<h2 class="widget-title subheading heading-size-3">',
+		'after_title'   => '</h2>',
+		'before_widget' => '<div class="widget %2$s"><div class="widget-content">',
+		'after_widget'  => '</div></div>',
+	);
+  
+  // Footer #3.
+	register_sidebar(
+		array_merge(
+			$shared_args,
+			array(
+				'name'        => __( 'Footer #3', 'powdermountain' ),
+				'id'          => 'sidebar-3',
+				'description' => __( 'Widgets in this area will be displayed in the third column in the footer.', 'powdermountain' ),
+			)
+		)
+	);
+
+}
+
+add_action( 'widgets_init', 'powdermountain_sidebar_registration' );
